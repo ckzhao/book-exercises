ec5bf91672ab70aa10a6e9f9622c81fc066ed61a
Cindy Zhao <cindy@MacBook-Pro-4.local>
2020-02-13 23:30:59 -0800

    Add Week 6

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -1,16 +1,18 @@
+#ch 18, exercise 1
 # load relevant libraries
 library("httr")
 library("jsonlite")
 
 # Be sure and check the README.md for complete instructions!
 
-
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
+source("apikey.R") # access all the code in that file and run it 
 
+query_param <- list("api_key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "500 Days of Summer"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,26 +22,40 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+uri <- paste0(base_uri, endpoint)
 
+query_params_list <- list("api-key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(uri, query = query_params_list)
+body <- content(response, "text", encoding = "UTF-8")
+data <- print(fromJSON(body))
 
 # What kind of data structure did this produce? A data frame? A list?
 
+#list
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+movie_reviews <- data[["movie_reviews"]]
+names(movie_reviews)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- data.frame(movie_reviews)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+headline <- reviews[["headline"]]
+short_sum <- reviews[["short_summary"]]
+full_article <- reviews[["link"]]
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+
+list_of_info <- paste0(headline, short_sum, full_article)
+print(list_of_info)
+
